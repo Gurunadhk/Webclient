892c0fe4dedc9529a89f4d5c454e4b43a90eca05
Rama Kaki <Rama.Gurunadha@sstech.us>
2022-12-09 15:03:26 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebClientApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
